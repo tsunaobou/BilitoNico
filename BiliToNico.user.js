6f74d16737c9e2fbc3e50f80131882965c5e3b56
tsunaobou <63852578+tsunaobou@users.noreply.github.com>
2021-11-21 03:40:16 +0900

    Update BiliToNico.user.js
    Add comment

--- BiliToNico.user.js
+++ BiliToNico.user.js
@@ -10,13 +10,12 @@
 // ==/UserScript==
 
 //なぜかF5を押さないと動かない問題がある
-window.addEventListener('DOMContentLoaded', function() {
-    let re = new RegExp('sm[0-9]{1,9}')
-    var id = document.getElementsByClassName("desc-info desc-v2")[0].innerText
-    const match = re.exec(id)
-    if(match[0]!=null){
+window.addEventListener('DOMContentLoaded', function() {    //ロード時に実行
+    let re = new RegExp('sm[0-9]{1,9}') //ニコ動の動画IDを正規表現パターンとする
+    var id = document.getElementsByClassName("desc-info desc-v2")[0].innerText  //動画説明欄を取得
+    const match = re.exec(id)   //正規表現でマッチ
+    if(match[0]!=null){ //マッチしてnullが返らなければ
         var link = "https://www.nicovideo.jp/watch/"+match[0]
-
-        window.open(link)
+        window.open(link)   //リンクを新しいタブで開く
     }
 })
