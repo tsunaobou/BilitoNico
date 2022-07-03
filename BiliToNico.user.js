78275ff51f9a1a32bfcc46e3cc7594e0c4e5b2b9
tsunaobou <63852578+tsunaobou@users.noreply.github.com>
2022-07-04 01:39:36 +0900

    fixed #2

--- BiliToNico.user.js
+++ BiliToNico.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         BiliToNico
+// @name         BiliToNicoTube
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Automatic Redirect from Bilibili to Niconico Source video.
+// @description  Automatic Redirect from Bilibili to Niconico or Youtube Source video.
 // @author       Tsunaobou
 // @match        https://www.bilibili.com/video/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -10,12 +10,19 @@
 // ==/UserScript==
 
 //なぜかF5を押さないと動かない問題がある
-window.addEventListener('DOMContentLoaded', function() {    //ロード時に実行
+window.addEventListener('DOMContentLoaded', function() { //ロード時に実行
     let re = new RegExp('sm[0-9]{1,9}') //ニコ動の動画IDを正規表現パターンとする
-    var id = document.getElementsByClassName("desc-info desc-v2")[0].innerText  //動画説明欄を取得
-    const match = re.exec(id)   //正規表現でマッチ
-    if(match[0]!=null){ //マッチしてnullが返らなければ
-        var link = "https://www.nicovideo.jp/watch/"+match[0]
-        window.open(link)   //リンクを新しいタブで開く
+    let re_second = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/; //Youtubeの動画URLを正規表現パターンとする
+    var id = document.getElementsByClassName("desc-info desc-v2")[0].textContent //動画説明欄を取得
+    const match = re.exec(id) //正規表現でマッチ
+    const match_second = re_second.exec(id) //正規表現でマッチ
+    if (match != null) { //マッチしてnullが返らなければ
+        var link = "https://www.nicovideo.jp/watch/" + match[0]
+        window.open(link) //リンクを新しいタブで開く
+        return;
+    } else if (match_second != null) { //nullが返ったら
+        var link_second = "https://www." + match_second[0]
+        window.open(link_second)
+        return;
     }
 })
--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-# BilitoNico
-Userscript for Automatic jump from Bilibili to Nicovideo.
+# BilitoNicoTube
+Userscript for Automatic Redirect from Bilibili to Niconico or Youtube Source video.
 
 ## 使い方
 1.Greasemonkey/Tampermonkeyなどでユーザースクリプト追加画面に入る      
-2.このコードをコピペする    
+2.このコードをコピペするか、コードブラウズ画面からRawを押してインストールする   
 3.Ctrl+Sで保存して、「設定」タブから実行のタイミングをdocument-startにする  
 4.Bilibiliの動画リンクにアクセスする    
-5.元動画ページに飛べる！    
+5.元動画ページ(Youtube,niconico)に飛べる！    
 
 ## 仕組み
 なんてことはない、単純設計です。    
-動画の説明欄にニコニコ動画の動画IDが載っていたらそのリンク先にジャンプするだけです。    
+動画の説明欄にニコニコ動画/Youtubeの動画IDが載っていたらそのリンク先にジャンプするだけです。    
 なぜだかBilibiliはacgというサイトに間違ってジャンプしてしまう仕様のようなので作ってみた次第です。   
 
+## 注意
+実行する前に必ず ユーザースクリプト＞設定＞全般＞実行のタイミング を**document-start**にしてください。    
+そうしないと動きません。
